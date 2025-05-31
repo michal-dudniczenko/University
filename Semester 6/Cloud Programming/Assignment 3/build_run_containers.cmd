fab49bc1124739f78739a763767934fb08a66be8
michal-dudniczenko <dudniczenkom@gmail.com>
2025-05-31 20:18:48 +0200

    cloud programming, ecommerce system docker deployment, zmianna nazw obrazow

--- Semester 6/Cloud Programming/Assignment 3/build_run_containers.cmd
+++ Semester 6/Cloud Programming/Assignment 3/build_run_containers.cmd
@@ -1,13 +1,15 @@
-docker build -f Dockerfile_UserService -t userservice .
-docker build -f Dockerfile_ProductService -t productservice .
-docker build -f Dockerfile_CartService -t cartservice .
-docker build -f Dockerfile_OrderService -t orderservice .
-docker build -f Dockerfile_NotificationService -t notificationservice .
+chdir C:\Users\Michal\Documents\Studia\Semester 6\Cloud Programming\Assignment 2\ECommerceSystem
 
-docker run -d -p 5001:5001 --network ecommerce-network --name userservice-app userservice
-docker run -d -p 5002:5002 --network ecommerce-network --name productservice-app productservice
-docker run -d -p 5003:5003 --network ecommerce-network --name cartservice-app cartservice
-docker run -d -p 5004:5004 --network ecommerce-network --name orderservice-app orderservice
-docker run -d -p 5005:5005 --network ecommerce-network --name notificationservice-app notificationservice
+docker build -f Dockerfile_UserService -t user-service .
+docker build -f Dockerfile_ProductService -t product-service .
+docker build -f Dockerfile_CartService -t cart-service .
+docker build -f Dockerfile_OrderService -t order-service .
+docker build -f Dockerfile_NotificationService -t notification-service .
+
+docker run -d -p 5001:5001 --network ecommerce-network --name user-service user-service
+docker run -d -p 5002:5002 --network ecommerce-network --name product-service product-service
+docker run -d -p 5003:5003 --network ecommerce-network --name cart-service cart-service
+docker run -d -p 5004:5004 --network ecommerce-network --name order-service order-service
+docker run -d -p 5005:5005 --network ecommerce-network --name notification-service notification-service
 
 pause
