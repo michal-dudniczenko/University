aad4057d44f55900f17d289f6d40a54c3f8e2a50
Michał Dudniczenko <m.dudniczenko@godeltech.com>
2026-05-14 16:04:41 +0200

    sem7/team-project - Clean up requests logging

--- Semester 7/Team Project/SoundmatesAPI/src/Soundmates.Api/Extensions/ControllerExtensions.cs
+++ Semester 7/Team Project/SoundmatesAPI/src/Soundmates.Api/Extensions/ControllerExtensions.cs
@@ -9,91 +9,35 @@ public static class ControllerExtensions
         this ControllerBase controller,
         Result<T> result)
     {
-        var logger = controller.HttpContext.RequestServices
-            .GetRequiredService(typeof(ILogger<>)
-            .MakeGenericType(controller.GetType()))
-            as ILogger;
-
-        ActionResult<T> response;
-        int statusCode;
-
         if (result.IsSuccess)
         {
-            response = controller.Ok(result.Value);
-            statusCode = 200;
-        }
-        else
-        {
-            (response, statusCode) = result.ErrorType switch
-            {
-                ErrorType.BadRequest => (controller.BadRequest(new { message = result.ErrorMessage }), 400),
-                ErrorType.Unauthorized => (controller.Unauthorized(new { message = result.ErrorMessage }), 401),
-                ErrorType.NotFound => (controller.NotFound(new { message = result.ErrorMessage }), 404),
-                ErrorType.InternalServerError or _ => (controller.StatusCode(500, new { message = result.ErrorMessage }), 500)
-            };
+            return controller.Ok(result.Value);
         }
 
-        if (statusCode != 200)
+        return result.ErrorType switch
         {
-            logger?.LogInformation("RESPONSE: {StatusCode} for {Controller}.{Action}: {ErrorMessage}",
-                statusCode,
-                controller.GetType().Name,
-                controller.ControllerContext.ActionDescriptor.ActionName,
-                result.ErrorMessage);
-        }
-        else
-        {
-            logger?.LogInformation("RESPONSE: {StatusCode} for {Controller}.{Action}",
-                statusCode,
-                controller.GetType().Name,
-                controller.ControllerContext.ActionDescriptor.ActionName);
-        }
-        return response;
+            ErrorType.BadRequest => controller.BadRequest(new { message = result.ErrorMessage }),
+            ErrorType.Unauthorized => controller.Unauthorized(new { message = result.ErrorMessage }),
+            ErrorType.NotFound => controller.NotFound(new { message = result.ErrorMessage }),
+            ErrorType.InternalServerError or _ => controller.StatusCode(500, new { message = result.ErrorMessage })
+        };
     }
 
     public static ActionResult ResultToHttpResponse(
         this ControllerBase controller,
         Result result)
     {
-        var logger = controller.HttpContext.RequestServices
-            .GetRequiredService(typeof(ILogger<>)
-            .MakeGenericType(controller.GetType()))
-            as ILogger;
-
-        ActionResult response;
-        int statusCode;
-
         if (result.IsSuccess)
         {
-            response = controller.Ok();
-            statusCode = 200;
-        }
-        else
-        {
-            (response, statusCode) = result.ErrorType switch
-            {
-                ErrorType.BadRequest => (controller.BadRequest(new { message = result.ErrorMessage }), 400),
-                ErrorType.Unauthorized => (controller.Unauthorized(new { message = result.ErrorMessage }), 401),
-                ErrorType.NotFound => (controller.NotFound(new { message = result.ErrorMessage }), 404),
-                ErrorType.InternalServerError or _ => (controller.StatusCode(500, new { message = result.ErrorMessage }), 500)
-            };
+            return controller.Ok();
         }
 
-        if (statusCode != 200)
+        return result.ErrorType switch
         {
-            logger?.LogInformation("RESPONSE: {StatusCode} for {Controller}.{Action}: {ErrorMessage}",
-                statusCode,
-                controller.GetType().Name,
-                controller.ControllerContext.ActionDescriptor.ActionName,
-                result.ErrorMessage);
-        }
-        else
-        {
-            logger?.LogInformation("RESPONSE: {StatusCode} for {Controller}.{Action}",
-                statusCode,
-                controller.GetType().Name,
-                controller.ControllerContext.ActionDescriptor.ActionName);
-        }
-        return response;
+            ErrorType.BadRequest => controller.BadRequest(new { message = result.ErrorMessage }),
+            ErrorType.Unauthorized => controller.Unauthorized(new { message = result.ErrorMessage }),
+            ErrorType.NotFound => controller.NotFound(new { message = result.ErrorMessage }),
+            ErrorType.InternalServerError or _ => controller.StatusCode(500, new { message = result.ErrorMessage })
+        };
     }
 }
--- Semester 7/Team Project/SoundmatesAPI/src/Soundmates.Api/Middleware/LogRequestInfoMiddleware.cs
+++ Semester 7/Team Project/SoundmatesAPI/src/Soundmates.Api/Middleware/LogRequestInfoMiddleware.cs
@@ -1,22 +1,20 @@
 ﻿namespace Soundmates.Api.Middleware;
 
-public class LogRequestInfoMiddleware
+public class LogRequestInfoMiddleware(
+    RequestDelegate next,
+    ILogger<LogRequestInfoMiddleware> logger)
 {
-    private readonly RequestDelegate _next;
-    private readonly ILogger<LogRequestInfoMiddleware> _logger;
-
-    public LogRequestInfoMiddleware(RequestDelegate next, ILogger<LogRequestInfoMiddleware> logger)
-    {
-        _next = next;
-        _logger = logger;
-    }
-
     public async Task InvokeAsync(HttpContext context)
     {
-        _logger.LogInformation("REQUEST: {Method} {Url}", 
-            context.Request.Method, 
+        logger.LogInformation("REQUEST: {Method} {Url}",
+            context.Request.Method,
             context.Request.Path + context.Request.QueryString);
 
-        await _next(context);
+        await next(context);
+
+        logger.LogInformation("RESPONSE: {StatusCode} for {Method} {Url}",
+                context.Response.StatusCode,
+                context.Request.Method,
+                context.Request.Path + context.Request.QueryString);
     }
 }
